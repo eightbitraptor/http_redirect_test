27a52e04b2d8ca34dd4df79c6b234c31042e8905
Matt House <matt@theshadowaspect.com>
2009-04-10 17:06:42 +0100

    add the facility to make permanent redirects global across an entire test

--- lib/http_redirect_test/http_redirect_test.rb
+++ lib/http_redirect_test/http_redirect_test.rb
@@ -3,11 +3,21 @@ require File.join(File.dirname(__FILE__), "resource_path")
 require 'test/unit'
 
 class HTTPRedirectTest < Test::Unit::TestCase
+  # permanent can be overriden in 2 ways, with a call to self.permanent to set globally. Or via the options array for each redirect call
+  @permanent=nil
 
   def default_test; end # placeholder to stop Test::Unit from complaining
 
   def self.domain=(domain)
     RedirectCheck.domain = domain
+  end
+  
+  def self.permanent=(permanent)
+    @permanent=permanent
+  end
+  
+  def self.permanent?
+    @permanent.nil? ? false : true
   end
 
   def self.should_not_redirect(path)
@@ -34,7 +44,7 @@ class HTTPRedirectTest < Test::Unit::TestCase
     source_path = ResourcePath.new(source, :param => 'subdir').to_s
     destination_path = ResourcePath.new(options[:to], :param => 'subdir').to_s
 
-    permanent = options.fetch(:permanent, true)
+    @permanent = options.fetch(:permanent, true) if @permanent.blank?
   
     class_eval <<-CODE
       def test_#{name_for(source_path)}_should_redirect_to_#{name_for(destination_path)}
@@ -43,7 +53,7 @@ class HTTPRedirectTest < Test::Unit::TestCase
         assert_equal '#{destination_path}', redirection.redirected_path,
         "'#{source_path}' is not redirecting to '#{destination_path}'"
         
-        if #{permanent}
+        if #{@permanent}
           assert_equal true, redirection.permanent_redirect?,
           "The redirection from '#{source_path}' to '#{destination_path}' doesn't appear to be a permanent redirect"
         end
